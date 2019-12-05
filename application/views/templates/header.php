02f6834f6a8f068a909e346cda6d302cead32a3b
FlowOverstack <ajdika@live.com>
2019-12-05 10:20:24 +0100

    Add bootstrap CDN and navbar to views/templates/header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,5 +3,22 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>CI Multi file upload</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/flatly/bootstrap.min.css">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<a class="navbar-brand" href="/">ciBlog</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarNav">
+		<ul class="navbar-nav">
+			<li class="nav-item active">
+				<a class="nav-link" href="<?php echo base_url(); ?>">Home</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
+			</li>
+		</ul>
+	</div>
+</nav>
