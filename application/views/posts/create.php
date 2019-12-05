120ebf4c5a4999b6e96a074b8b88f2c698c7f9d5
FlowOverstack <ajdika@live.com>
2019-12-05 15:33:01 +0100

    Improve the forms on create post view

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -3,13 +3,13 @@
 <form>
 	<div class="form-group">
 		<label>Title</label>
-		<input type="text" class="form-control" aria-describedby="emailHelp"
-			   placeholder="Add title" name="title">
-		<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+		<input type="text" class="form-control" placeholder="Add title" name="title" value="<?php echo $post['title']; ?>">
 	</div>
 	<div class="form-group">
 		<label>Body</label>
-		<textarea class="form-control" placeholder="Add Body" name="body"></textarea>
+		<textarea class="form-control" placeholder="Add Body" name="body">
+			<?php echo $post['body']; ?>
+		</textarea>
 	</div>
 	<button type="submit" class="btn btn-default">Submit</button>
 </form>
