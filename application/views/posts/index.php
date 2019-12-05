c8a2aee446f7c917d4ca5ae81c66d30a909a41c7
FlowOverstack <ajdika@live.com>
2019-12-05 11:41:13 +0100

    Add 'Read more' button that has the $slug echoed out in the anchor tag

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -5,4 +5,5 @@
 	<h3><?php echo $post['title']; ?></h3>
 	<small class="badge badge-primary d-incline-block">Posted on: <?php echo $post['created_at']; ?></small><br>
 	<?php echo $post['body']; ?>
+	<p><a class="btn btn-success" href="<?= site_url('/posts/'.$post['slug']) ; ?>">Read More</a></p>
 <?php endforeach; ?>
