730d8ba4120c0fce67b6f4333021e4c0a7fe2ce4
FlowOverstack <ajdika@live.com>
2019-12-05 12:10:37 +0100

    Revert to $posts variable

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -1,11 +1,10 @@
 <pre>
-<?php print_r($post); ?>
-<?php var_dump($post); ?>
+<?php // print_r($post); ?>
+<?php // var_dump($post); ?>
 </pre>
 <h2><?php echo $post['title']; ?></h2>
 
-<small class="badge badge-primary d-inline-block">Posted on: <?php echo $post['created_at'];
-	?></small><br>
+<small class="badge badge-primary d-inline-block">Posted on: <?php echo $post['created_at']; ?></small><br>
 <div class="d-block p-2">
 	<?= $post['body']; ?>
 </div>
