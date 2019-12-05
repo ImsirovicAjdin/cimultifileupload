74a5a44fe37fd15cd4c2065d3257c19e05577e40
FlowOverstack <ajdika@live.com>
2019-12-05 14:43:10 +0100

    Add create_post ACTION to Post_model

--- application/models/Post_model.php
+++ application/models/Post_model.php
@@ -13,4 +13,19 @@
 			$query = $this->db->get_where('posts', array('slug' => $slug));
 			return $query->row_array();
 		}
+
+		public function create_post(){
+			$slug = url_title($this->input->post('title')); // this is how we get the form values
+			// and we want the value of title - so we're just wrapping the ('title') in this
+			// url_title function, which turns it into a slug
+			$data = array(
+				'title' => $this->input->post('title'), // we're creating a 'title' key in the
+				// $data array, and we're setting it to whatever came in from
+				// $_POST['<name="title"> form field']
+				'slug' => $slug, // the slug will come from the slug variable we just created,
+				'body' => $this->input->post('body')
+			); // so there's our array, we still need to pass it TO DB, using RETURN stmt below:
+			return $this->db->insert('posts', $data);
+		}
+
 	}
