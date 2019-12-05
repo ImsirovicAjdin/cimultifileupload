13f0cd60e084915e3e505d56613d7ffa5addbd8c
FlowOverstack <ajdika@live.com>
2019-12-05 11:24:18 +0100

    Add get_posts action to post_model

--- application/models/Post_model.php
+++ application/models/Post_model.php
@@ -4,4 +4,13 @@
 		{
 			$this->load->database();
 		}
+
+		public function get_posts($slug = FALSE){
+			if($slug === FALSE){
+				$query = $this->db->get('posts'); // we're using active model here
+				return $query->result_array();
+			}
+			$query = $this->db->get_where('posts', array('slug' => $slug));
+			return $query->row_array();
+		}
 	}
