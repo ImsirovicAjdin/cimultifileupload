34e71ea21eaaaa4b36fbbf90d61b48c04cf82e2a
FlowOverstack <ajdika@live.com>
2019-12-05 14:40:25 +0100

    Fix duplicate call to header and footer in Posts CTRL, create Action

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -30,9 +30,6 @@ class Posts extends CI_Controller {
 
 	public function create() {
 		$data['title'] = 'Create Post';
-		$this->load->view('templates/header');
-		$this->load->view('posts/create', $data);
-		$this->load->view('templates/footer');
 
 		$this->form_validation->set_rules('title', 'Title', 'required');
 		$this->form_validation->set_rules('body', 'Body', 'required');
