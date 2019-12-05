807acf95cc0c91b9cc24528299b6d4ee0da82bca
FlowOverstack <ajdika@live.com>
2019-12-05 11:39:13 +0100

    Run foreach loop on all posts, show post title, post created_at (in a badge), post body

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,3 +1,8 @@
 <h1 style="padding-top: 20px">
 	<?= $title ?>
 </h1>
+<?php foreach($posts as $post) : ?>
+	<h3><?php echo $post['title']; ?></h3>
+	<small class="badge badge-primary d-incline-block">Posted on: <?php echo $post['created_at']; ?></small><br>
+	<?php echo $post['body']; ?>
+<?php endforeach; ?>
